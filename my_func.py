de061ecf7f07ba9f0ca713a1e1b3e3dcccc16dda
Austin-Marfo <austin.marfo123@gmail.com>
2021-12-07 11:56:34 +0000

    Added my function file

---
+++ my_func.py
@@ -0,0 +1,56 @@
+# def add_numbers(x,y):
+#     addition = x + y
+#     print('Result is ', addition)
+# add_numbers(34,44)
+
+# def sub_num(a,b):
+#     subtraction = a-b
+#     print('Result is ', subtraction)
+# sub_num(76,43)
+
+# def greetings(name):
+#     print('Helllo, ',name)
+#     print('How are you doing today?')
+# greetings('Austin')
+
+# def mul(e,f):
+#     values = e*f
+#     x = values
+#     print(x)
+# mul(2,4)
+
+# marks=(34,63,77,53,78,97,78,34)
+
+# def average_mark():
+#     num_of_subjects = len(marks)
+#     total_marks = sum(marks)
+#     avg_marks =  total_marks / num_of_subjects  
+#     print('The average score is ', avg_marks)
+# average_mark()
+
+
+# lst = [23,42,23,55,34,66,74,54,34,65,24,64,54,77,46]
+# def find_less_num(lst):
+#     for a in lst:
+#         if a<=40:
+#             print(a)
+#         else:
+#             print('invalid')
+# find_less_num(lst)
+
+n= [4,24,68,456,17,79,16,3,474,97,63,79,519,84,12,21,11]
+# def odd_even_numbers():
+#     for o in n :
+#         odd=o%2
+#         if odd==1:
+#             print(o, 'is an odd number')
+
+#         else:
+#             print(o, 'is an even number')
+# odd_even_numbers()
+        
+def prime_num():
+    for p in n:
+        if p % 2 == 1:
+            print(p)
+prime_num()
